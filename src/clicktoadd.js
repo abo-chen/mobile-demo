4d8fc29290f68570d788684948cea879cd76997e
Abel Chen <abo-chen@users.noreply.github.com>
2023-10-29 12:20:42 -0600

    Encapsulated buttons
    Encapsulated variable color buttons

--- src/clicktoadd.js
+++ src/clicktoadd.js
@@ -1,26 +1,44 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import CustomButton from "./custombutton";
 
 export default ClickToAdd = () => {
   const [count, setCount] = React.useState(0);
   return (
     <View style={styles.container}>
       <Text style={styles.testStyle}>{count}</Text>
-      <Button onPress={() => setCount(count + 1)} title="Click to add"/>
+      <CustomButton
+        label="Click to add"
+        onPressFun={() => setCount(count + 1)}
+        buttonStyle={styles.buttonStyle}
+        textStyle={styles.buttontextStyle}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-
     backgroundColor: "#fff",
     alignItems: "left",
     justifyContent: "center",
-    width: "33%",
+    //width: "33%",
   },
-  testStyle: {
-    color: "red",
-    fontSize: 50,
+  buttonStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 6,
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: "green",
+    width: 90,
+  },
+  buttontextStyle: {
+    fontSize: 9,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
 });
---
+++ src/custombutton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Pressable, Text, StyleSheet } from 'react-native';
+
+const CustomButton = ({
+  initialButtonColor = 'green',
+  pressedButtonColor = 'grey',
+  initialTextColor = 'white',
+  pressedTextColor = 'yellow',
+  onPressFun,
+  label,
+  buttonStyle = {},
+  textStyle = {}
+}) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        buttonStyle,
+        {
+          backgroundColor: pressed ? pressedButtonColor : initialButtonColor
+        }
+      ]}
+      onPress={onPressFun}
+    >
+      {({ pressed }) => (
+        <Text style={[textStyle,{ color: pressed ? pressedTextColor : initialTextColor }]}>
+          {label}
+        </Text>
+      )}
+    </Pressable>
+  );
+};
+
+export default CustomButton;
