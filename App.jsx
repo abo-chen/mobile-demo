030d856c5fb9ce2b66b3046074e62b0c6425a8f1
Abel Chen <abo-chen@users.noreply.github.com>
2023-11-12 11:01:23 -0700

    Lab 3
    For the sake of experimentation, there is no separation of the ToDoList into individual files, all Lab3 is in App.jsx.

--- App.jsx
+++ App.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  SafeAreaProvider,
-} from "react-native-safe-area-context";
-import ToDoList from "./src/ToDoList";
-import ToDoForm from "./src/ToDoForm";
+import React, { useState } from "react";
+import { StyleSheet, Pressable, Text, View, ScrollView } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+const App = () => {
+  const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+
+  function ToDoList({ tasks }) {
+    return (
+      <ScrollView>
+        {tasks.map((task, index) => (
+          <Pressable key={index}>
+            <View style={[styles.task]}>
+              <Text style={styles.taskText}>{task}</Text>
+            </View>
+          </Pressable>
+        ))}
+      </ScrollView>
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <ToDoList />
-        <ToDoForm />
+        <ToDoList tasks={tasks} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
-}
+};
 
+export default App;
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  completed: {
+    backgroundColor: "#e0e0e0",
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
